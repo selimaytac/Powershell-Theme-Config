4e228599402a24c76519cead206d8c24dfaf8b52
selim <53125635+selimaytac@users.noreply.github.com>
2022-10-21 08:41:24 +0300

    Listview and History prediction added.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,9 @@
 oh-my-posh init pwsh --config THEME.PATH | Invoke-Expression
-Import-Module -Name Terminal-Icons
 Import-Module PSReadLine
+Import-Module -Name Terminal-Icons
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadLineKeyHandler -Key Tab -Function Complete
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -Colors @{emphasis = '#ff0000'; inlinePrediction = 'blue'}
